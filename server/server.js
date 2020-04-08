6796817ccc7888abf472bb70a0dac2a5ecd99597
Haorong Sun <haorong4@illinois.edu>
2020-04-07 22:26:28 -0500

    haorong

--- server/server.js
+++ server/server.js
@@ -13,12 +13,12 @@ const handle = app.getRequestHandler();
 /**
  *  Code to test connection to MySQL. You should see "The solution is: 2" in your terminal.
  */
-// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('The solution is:', rows[0].solution);
-// })
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) {
+        throw err;
+    }
+    console.log('The solution is:', rows[0].solution);
+})
 
 app.prepare()
     .then(() => {
