dc15c172e3a2ab5eccd1c9c14746f678c79bc638
Haorong Sun <haorong4@illinois.edu>
2020-04-09 13:49:15 -0500

    a

--- server/api/routes.js
+++ server/api/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const secrets = require("../secrets.js");
 const router = express.Router();
-
+const connection = mysql.createConnection(secrets.getSqlCredentials());
 
 router.get('/test', (req, res) => {
     res.json({ data: "Hello World" });
@@ -15,11 +15,20 @@ router.post('/find', (req, res) => {
     var maxYear = 2000;
     var str_min = minYear.toString();
     var str_max = maxYear.toString();
-    // res.status(200);
-    //console.log("SELECT * FROM extremePoverty WHERE year > " + str_min + " AND year < " + str_max + " AND entity = \"" + country + "\"");
-    var result = searchConnection("SELECT * FROM extremePoverty WHERE year > " + str_min + " AND year < " + str_max + " AND entity = \"" + country + "\"");
-    res.send(result);
-    // Find rows here using the range [minYear, maxYear]
+
+    var query = "SELECT * FROM extremePoverty WHERE year > " + str_min + " AND year < " + str_max + " AND entity = \"" + country + "\""
+    connection.connect();
+    var data = "";
+    connection.query(query, function (err, result, fields) {
+        if (err) {
+            return "error";
+        }
+        for (var i = 0; i < result.length; i++){
+            data += result[i].entity + " " + result[i].code + " " + result[i].year + " " + result[i].percentExtremePoverty + "\n";
+        }
+        console.log(data);
+        res.send(data);
+    });
 });
 
 router.post('/insert', (req, res) => {
@@ -32,10 +41,8 @@ router.post('/insert', (req, res) => {
     var str_year =  year.toString() + ",";
     var str_percent =  percent.toString() + ")";
     var result = updateConnection("INSERT INTO extremePoverty VALUES " + str_con + str_code + str_year + str_percent);
-    res.send(result);
-    // res.status(200);
+    res.status(200);
     // Insert row into database here
-    console.log(req);
 });
 
 router.post('/update', (req, res) => {
@@ -67,25 +74,27 @@ router.post('/delete', (req, res) => {
 module.exports = router;
 
     // testConnection("SELECT * FROM extremePoverty WHERE year > 1980 AND year < 2000 AND country = \"United States\"");
-function searchConnection(query) {
-    connection = mysql.createConnection(secrets.getSqlCredentials());
-    connection.connect();
-    var res = "";
-    connection.query(query, function (err, result, fields) {
-        // if any error while executing above query, throw error
-        if (err) {
-            connection.end();
-            return error;
-        }
-        // if there is no error, you have the result
-        for (var i = 0; i < result.length; i++){
-            res += result[i].entity + " " + result[i].code + " " + result[i].year + " " + result[i].percentExtremePoverty + "\n";
-        }
-        console.log(res);
-        connection.end();
-        return res;
-    });
-}
+// function searchConnection(query) {
+//     connection = mysql.createConnection(secrets.getSqlCredentials());
+//     connection.connect();
+//     var res = connection.query(query, function (err, result, fields) {
+//         // if any error while executing above query, throw error
+//         if (err) {
+//             // connection.end();
+//             return "error";
+//         }
+//         // if there is no error, you have the result
+//         for (var i = 0; i < result.length; i++){
+//             res += result[i].entity + " " + result[i].code + " " + result[i].year + " " + result[i].percentExtremePoverty + "\n";
+//         }
+//         return res;
+//     });
+//     // const res = await connection.query(query);
+//     connection.end();
+//     console.log(res);
+
+//     // return res;
+// }
 
 
 function updateConnection(query) {
